9f67d7d08022c1b71c7bcd4ec101426e34659f47
Filip Hajdyła <hajdylaf@gmail.com>
2022-05-18 06:37:09 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import random as r
+import sys
+
+with open('pytaniaOWI.txt', encoding='utf-8') as f:
+    lines = f.readlines()
+    for idx, line in enumerate(lines):
+        lines[idx] = line.strip('\n')
+
+    Q = []
+    while lines:
+        Q.append(lines[:4])
+        lines = lines[4:]
+
+A = 'bcabacbababcabcbbcababcababcabcabacabcaabcbacabcbacabsbbbcscbababcabacbabaabcaababcabacbacbcabbabababacaacabcaaaacbababacbabcababbacbcabaababbbcababcabbcaabacab'
+asked = []
+
+
+def ask(n, Q):
+    q_num = r.randint(0, n)
+    if q_num not in asked:
+        for line in Q[q_num]:
+            print(line)
+        answer = input('Podaj odpowiedź (a/b/c): ').lower()
+    return q_num, answer
+
+
+def check_answer(q_num, answer, A):
+    if answer == A[q_num]:
+        return True
+    else:
+        return False, A[q_num]
+
+
+while True:
+    q_num, answer = ask(160, Q)
+    asked.append(q_num)
+    verdict, correct = check_answer(q_num, answer, A)
+    if verdict:
+        print('Gooooood, Anakin, good...')
+    else:
+        print('YOU WERE SUPPOSE TO DESTROY THE SITH NOT JOIN THEM!')
+        print(f'Prawidłowa odpowiedź to: {correct}')
+    if input('Type any key to continue\nType q to exit\n') in ('q', 'Q'):
+        sys.exit()
+    else:
+        continue
